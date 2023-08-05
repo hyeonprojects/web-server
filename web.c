372aad118fd4d383aa7445f5f43c0901f1bc84f3
@coffeehouse-DongHyeoncoffeehouse-DongHyeon <dh.lee@socialinvestinglab.com>
2023-08-05 14:01:45 +0900

    c언어 이해와 socket programming 이해

---
+++ web.c
@@ -0,0 +1,26 @@
+#inlcude <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handing(char *message);
+
+int main(int argc, char *argv[]) {
+    int serv_sock;
+    int clnt_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+
+    if(argc != 3) {
+        printf("Usage: %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sock=socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1) {
+        error_handing("socket() error");
+    }
+}
